7b62c441e6c62901a9060600a8f522d9486d12e8
gittenger <john.pittenger86@gmail.com>
2020-10-05 13:09:56 -0400

    user controller, query features

--- _controllers/_handlerFactory.js
+++ _controllers/_handlerFactory.js
@@ -1,11 +1,28 @@
 const catchAsync = require('../utils/catchAsync');
+const APIFeatures = require('../utils/APIFeatures');
 
 exports.getAll = Model =>
   catchAsync(async (req, res, next) => {
-    const docs = await Model.find();
+    //for nested routes
+    //ie: /model/modelId/reviews
+    let filter = {};
+    if (req.params.modelId) filter = { tour: req.params.modelId };
+
+    //Build query, returns "this" query
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    //Execute query
+    const docs = await features.query;
+    //use explain() to get detailed results for performance evaluation
+    // const docs = await features.query.explain();
 
     res.status(200).json({
       status: 'success',
+      requestedAt: req.requestTime,
       results: docs.length,
       data: { docs },
     });
@@ -23,6 +40,7 @@ exports.getOne = Model =>
 
     res.status(200).json({
       status: 'success',
+      requestedAt: req.requestTime,
       data: { data: doc },
     });
   });
@@ -36,5 +54,38 @@ exports.createOne = Model =>
       data: {
         data: doc,
       },
+    });
+  });
+
+exports.updateOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.deleteOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
     });
   });
--- _controllers/user.controller.js
+++ _controllers/user.controller.js
@@ -1,11 +1,63 @@
 const User = require('./../_models/user.model');
+const catchAsync = require('../utils/catchAsync');
 const handlers = require('./_handlerFactory');
 
-const { getAll, getOne } = handlers;
+const { getAll, getOne, updateOne, deleteOne } = handlers;
+
+//create filtered object to filter thru select fields. for filtering allowed updates
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(field => {
+    if (allowedFields.includes(field)) newObj[field] = obj[field];
+  });
+  return newObj;
+};
+
+//middleware to pass currently logged in user ID to getOne
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
 
 // CRUD
 exports.getAllUsers = getAll(User);
 exports.getUser = getOne(User);
+exports.updateUser = updateOne(User);
+exports.deleteUser = deleteOne(User);
+
+// Special CRUD
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //create error if user POSTS password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+
+  //update user document
+  //only allow select properties to be changed, filter unwanted object fields
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  //new option will return the new, updated object
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
 
 // UNDEFINED
 exports.createUser = (req, res) => {

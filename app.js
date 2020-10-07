ea9d8ff53789337b5fa3d6a2bcb83e89086230f1
gittenger <john.pittenger86@gmail.com>
2020-10-06 20:59:40 -0400

    security settings in app

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+
+//SECURITY
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
+//ERROR HANDLERS
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./_controllers/errorController');
+
+//ROUTERS
 const userRouter = require('./routes/user.routes');
 
+//
+//INITIALIZE
 const app = express();
 
-// MIDDLEWARES
+//
+// GLOBAL MIDDLEWARES
 app.use(morgan('dev'));
+// request time field for use in responses
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 app.use(
   bodyParser.json({
     limit: '10kb',
@@ -14,7 +35,47 @@ app.use(
 );
 app.use(cors());
 
+// SECURITY MIDDLEWARE
+
+// SET SECURITY HTTP HEADERS
+app.use(helmet());
+
+// RATE LIMITER --100/hr
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message:
+    'Too many requests from this IP address. Please try again in one hour.',
+});
+app.use('/api', limiter);
+
+// NOSQL QUERY INJECTION
+app.use(mongoSanitize());
+
+// XSS ATTACKS -- DATA SANITIZATION
+app.use(xss());
+
+// PARAMS POLLUTION
+app.use(
+  hpp({
+    whitelist: [
+      // put allowed rest parameters here
+    ],
+  })
+);
+
+// SERVING STATIC FILES
+app.use(express.static(`${__dirname}/_public`));
+
 // MOUNTING ROUTES MIDDLEWARES
 app.use('/api/users', userRouter);
+
+//ALL UNHANDLED ROUTES
+//handle all verbs using app.all, * = everything
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
